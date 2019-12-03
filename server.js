f143692593a3216eb9d890cdb30d9dac3a03f8f7
Camilo <cacevedo@lean-tech.io>
2019-12-03 16:55:44 -0500

    Server and client separated

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
+//*****************************/
 const net = require('net');
 const port = 9000;
-
-//--Client Instance
-let client = new net.Socket();
-client.connect(port, '127.0.0.1', () => {
-    console.log('Connected');
-});
-
-    //--Handling send data to the server - process.stdin used to write text and send it to the server
-process.stdin.on('data', (data) => {
-    client.write(data);
-    //client.destroy();
-});
-
-client.on('data', (data) => {
-    process.stdout.write(data);
-});
-    //--Handling error with the client connection
-client.on('error', (err) => {
-    console.log(err.message);
-});
-
-//*****************************/
 
 //-- Server Instance
 
